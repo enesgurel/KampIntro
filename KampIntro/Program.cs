561075686d7ca4878a558c95165135615aa35dcf
Enes <enesrufgurel@gmail.com>
2021-07-13 12:24:34 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety
+            // Do not repeat yourself
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
---
+++ Matematik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Matematik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dosrtIslem = new DortIslem();
+            dosrtIslem.Topla(3, 5);
+
+            dosrtIslem.Topla(6, 9);
+        }
+    }
+}
