b7c9a96bc9f609e3148752b27f6a9d94a78576f3
Enes <enesrufgurel@gmail.com>
2021-12-03 00:14:15 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            // type-safe -- tip güvenliği
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine("----------------");
+            }
+
+            Console.WriteLine("--------------------------Metodlar--------------------------");
+
+            //instance - örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 12, 10);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 12, 10);
+
+        }
+    }
+}
+
+
+
+// Dont repeat yourself - DRY  - Clean code - Best practice
