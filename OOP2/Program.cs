1e181ca8f8ddd68024e019314bacb06906756ab9
Enes <enesrufgurel@gmail.com>
2022-02-03 00:11:32 +0300

    Dersin ilk yarısı

---
+++ OOP2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Engin demiroğ
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12354";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12345678910";
+
+            // Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "543121";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "12345678910";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(musteri1);
+            customerManager.Ekle(musteri2);
+
+        }
+    }
+}
